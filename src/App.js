9217200e36585e966105bc953cbe04e150017733
Vojtech Simetka <vojtech@simetka.cz>
2021-03-01 13:28:20 +0100

    chore: added bee-js with yarn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { Bee } from '@ethersphere/bee-js'
 
 function App() {
+  const bee = new Bee('https://gateway.ethswarm.org/')
   return (
     <div className="App">
       <header className="App-header">
